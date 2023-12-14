99bff5307ccd71324dd7c8ce6546e288bed8d3ac
ra5pvt1n <ra5pvt1n@proton.me>
2023-12-14 18:49:38 +0100

    add logo backlink

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,23 +1,27 @@
 <script>
-	import { PUBLIC_LOGO } from "$env/static/public";
-	import Fees from "./Fees.svelte";
-    import Mempool from "./Mempool.svelte";
-	import Price from "./Price.svelte";
-	import Time from "./Time.svelte";
+  import { PUBLIC_LOGO } from "$env/static/public";
+  import Fees from "./Fees.svelte";
+  import Mempool from "./Mempool.svelte";
+  import Price from "./Price.svelte";
+  import Time from "./Time.svelte";
 </script>
 
-<header class="relative py-6 mb-6 gap-3 items-center justify-between rounded-full">
-    <div class="flex items-center pl-4 font-mono text-lg font-bold text-gray-200">
-        <img src={PUBLIC_LOGO} class="h-12 mr-3" alt="logo" />
-        <div class="sm:flex items-center gap-4 hidden">
-            <Time/>
-        </div>
-    </div>
-    <a href="https://mempool.space/">
-        <div class="absolute top-0 right-0 px-6 py-6 text-right">
-            <Mempool/>
-            <Fees/>
-            <Price/>
-        </div>
+<header
+  class="relative py-6 mb-6 gap-3 items-center justify-between rounded-full"
+>
+  <div class="flex items-center pl-4 font-mono text-lg font-bold text-gray-200">
+    <a href=".">
+      <img src={PUBLIC_LOGO} class="h-12 mr-3" alt="logo" />
     </a>
+    <div class="sm:flex items-center gap-4 hidden">
+      <Time />
+    </div>
+  </div>
+  <a href="https://mempool.space/">
+    <div class="absolute top-0 right-0 px-6 py-6 text-right">
+      <Mempool />
+      <Fees />
+      <Price />
+    </div>
+  </a>
 </header>
