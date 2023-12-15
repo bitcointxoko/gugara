b7fbe9244cf3b45b591071fd94f58f5694599d8b
ra5pvt1n <ra5pvt1n@proton.me>
2023-12-15 01:47:35 +0100

    add carousel

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,12 +1,22 @@
 <script>
+  import { browser } from "$app/environment";
+  let carousel; // for calling methods of the carousel instance
+  const handleNextClick = () => {
+    carousel.goToNext();
+  };
+  import Carousel from "svelte-carousel";
   import { _ } from "../../services/i18n";
   import { PUBLIC_PRICE_CURRENCY_SYMBOL } from "$env/static/public";
 
   let products = [
     {
       name: "Calcetines",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/Storage/27156195-7ac7-462d-9271-d04b0031a36f",
+        "https://btcpay.bitcointxoko.com/Storage/55322c67-0021-4560-a455-fd9cb53e3992",
+        "https://btcpay.bitcointxoko.com/Storage/21864421-128a-4cc8-af59-6079f7393613",
+        "https://btcpay.bitcointxoko.com/Storage/f825d8ae-7d1a-4fde-beb3-3e22cc2b8b24",
+      ],
       description:
         "¡Ponte a la moda Bitcoin con nuestros calcetines de Bitcoin! 🧦 ¡Da pasos seguros con estilo mientras muestras tu amor por Bitcoin! 🚀",
       action:
@@ -17,8 +27,9 @@
     },
     {
       name: "Calcetines x 3",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/Storage/e4fdc158-6d5e-46fb-b128-fef49a3d099a",
+      ],
       description:
         "Presentamos nuestros nuevos calcetines Bitcoin, la forma más cómoda de caminar hacia el futuro financiero. 🧦 ¡Lleva consigo el espíritu de Satoshi en cada paso! 🚀 ",
       action:
@@ -29,8 +40,9 @@
     },
     {
       name: "Tarjeta NFC",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/LocalStorage/0b8333f0-1267-421a-97cd-69a44490b34a-nfc-card-ntag424-dna.jpg",
+      ],
       description:
         "Un chip NXP NTAG424 DNA dentro de una tarjeta NFC PVC blanca. ",
       action:
@@ -41,8 +53,16 @@
     },
     {
       name: "Kit BTClock",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/Storage/8b6d0541-bd0b-4713-b674-bff4fc40c37a",
+        "https://btclock.store/wp-content/uploads/2023/11/webinterface.png",
+        "https://btclock.store/wp-content/uploads/2023/08/PresolderAll.jpeg",
+        "https://btclock.store/wp-content/uploads/2023/08/AcrylSet.jpeg",
+        "https://btclock.store/wp-content/uploads/2023/08/FrontPCB.jpeg",
+        "https://btclock.store/wp-content/uploads/2023/08/PresolderSMD.jpeg",
+        "https://btclock.store/wp-content/uploads/2023/10/img_1805-scaled.jpeg",
+        "https://btclock.store/wp-content/uploads/2023/10/img_1804-scaled.jpeg",
+      ],
       description:
         "Incluye PCB principal (componentes SMB soldados), PCB frontal, parte frontal y trasera acrílicas, cable USB-C, juego de tornillos, componentes necesarios. No incluye pantallas de tinta electrónica ni S3 mini. ",
       action:
@@ -53,8 +73,9 @@
     },
     {
       name: "Pantallas e-Ink (x 7)",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/Storage/1952523a-fe90-4aff-9091-1989cdd43fa1",
+      ],
       description:
         "Pantallas tinta eletrónica para BTClock. Necesitas 7 por reloj. El precio es por 7 pantallas. ",
       action:
@@ -65,8 +86,9 @@
     },
     {
       name: "S3 mini",
-      image:
+      images: [
         "https://btcpay.bitcointxoko.com/Storage/ec028167-3684-485e-9b6d-611067ef5bec",
+      ],
       description: "S3 mini para BTClock. ",
       action:
         "https://btcpay.bitcointxoko.com/apps/315345vxVqUWLASyga7ZLW3tdxUa/pos",
@@ -85,9 +107,27 @@
       <div
         class="p-2 sm:p-4 md:p-6 gap-4 rounded-lg border border-solid hover:border-bright bg-card-bright font-mono max-w-lg text-muted-bright"
       >
-        <img src={product.image} alt={product.name} class="mt rounded-md" />
-        <h3 class="mx my-2 font-semibold text-left">{product.name}</h3>
-        <p class="mx-2 my-2">{product.description}</p>
+        {#if product.images.length > 1}
+          {#if browser}
+            <Carousel bind:this={carousel}>
+              {#each product.images as image}
+                <div class="mt-2 relative overflow-hidden pb-4/5">
+                  <img
+                    src={image}
+                    alt={product.name}
+                    class="rounded-md absolute h-full w-full object-cover"
+                  />
+                </div>
+              {/each}
+            </Carousel>
+          {/if}
+        {:else}
+          <div class="px-2 mt-2">
+            <img src={product.images} alt="" class="rounded-md" />
+          </div>
+        {/if}
+        <h3 class="mx-2 my-2 font-semibold text-left">{product.name}</h3>
+        <p class="mx-3 my-2">{product.description}</p>
         <div class="relative flex justify-center">
           <form method="POST" action={product.action}>
             <button
