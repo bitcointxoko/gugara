4883772c3b0cb559d280192e9764100824b7c4cd
rasputin <ra5pvt1n@proton.me>
2024-03-24 10:15:43 +0100

    add pwa

--- src/components/LinkList.svelte
+++ src/components/LinkList.svelte
@@ -6,16 +6,14 @@
 	export let tags: string[] | string[][] = [];
 </script>
 
-<div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-	{#each tags as tag}
-		<Card href={tag[1]}>
-			<h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-				{tag[2]}
-			</h5>
-			<a href={tag[1]} class="mt-3 inline-flex items-center text-primary-600 hover:underline">
-				{$_('apps.visit')}
-				<ArrowUpRightFromSquareOutline class="ms-2.5 h-3 w-3" />
-			</a>
-		</Card>
-	{/each}
-</div>
+{#each tags as tag}
+	<Card href={tag[1]}>
+		<h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			{tag[2]}
+		</h5>
+		<a href={tag[1]} class="mt-3 inline-flex items-center text-primary-600 hover:underline">
+			{$_('apps.visit')}
+			<ArrowUpRightFromSquareOutline class="ms-2.5 h-3 w-3" />
+		</a>
+	</Card>
+{/each}
--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -2,6 +2,7 @@
 	import { _ } from '../../services/i18n';
 	import ndk, { user } from '$lib/stores/ndk';
 	import LinkList from '../../components/LinkList.svelte';
+	import { Card } from 'flowbite-svelte';
 
 	const eventsPromise = ndk.fetchEvents({
 		kinds: [30003],
@@ -9,10 +10,38 @@
 	});
 </script>
 
-<main class=" mx-4 my-4 flex flex-row flex-wrap items-start justify-center">
-	{#await eventsPromise then events}
-		{#each Array.from(events).slice(0, 1) as event}
-			<LinkList tags={event.tags.filter((v) => v[0] === 'r')} />
-		{/each}
-	{/await}
+<main class="mx-4 my-4 flex flex-row flex-wrap items-start justify-center gap-2">
+	<div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+		{#await eventsPromise}
+			<Card size="xs">
+				<div class="mb-2.5 h-2.5 w-24 rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+			</Card>
+			<Card size="xs">
+				<div class="mb-2.5 h-2.5 w-24 rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+			</Card>
+			<Card size="xs">
+				<div class="mb-2.5 h-2.5 w-24 rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+			</Card>
+			<Card size="xs">
+				<div class="mb-2.5 h-2.5 w-24 rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+			</Card>
+			<Card size="xs">
+				<div class="mb-2.5 h-2.5 w-24 rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+				<div class="mt-2 h-2 w-32 animate-pulse rounded-full bg-emerald-500"></div>
+			</Card>
+		{:then events}
+			{#each Array.from(events).slice(0, 1) as event}
+				<LinkList tags={event.tags.filter((v) => v[0] === 'r')} />
+			{/each}
+		{/await}
+	</div>
 </main>
