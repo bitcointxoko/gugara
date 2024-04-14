76c5f30434754c13aa9fe42b302ec117bcb7b4cb
rasputin <ra5pvt1n@proton.me>
2024-04-14 15:07:06 +0200

    add user folder

---
+++ src/components/user/Following.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { NDKUser } from '@nostr-dev-kit/ndk';
+	import { Spinner } from 'flowbite-svelte';
+
+	export let user: NDKUser;
+
+	let follows = user.follows();
+</script>
+
+{#await follows}
+	<Spinner size={4} />
+{:then users}
+	{users.size}
+{/await}
