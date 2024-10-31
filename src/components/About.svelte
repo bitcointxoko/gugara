10e7d4c8321fd182d502339f4f8171bcdc876ab8
rasputin <115251263+ra5pvt1n@users.noreply.github.com>
2024-10-31 19:18:57 +0100

    Update About.svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -10,24 +10,19 @@
 
 <Card size="lg" class="bg-warm-50">
 	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-		Ongi etorri! 🤙
+		{$_("about.welcome")}
+		
 	</h5>
 	<p class="font-normal leading-tight text-gray-700 dark:text-gray-400">
 		<a id="popover-0" href="/u/{user.npub}"
 			><span class="font-bold decoration-dotted hover:underline">{PUBLIC_NAME}</span>
 		</a>
-		es un espacio abierto de intercambio y aprendizaje. Surgió de manera bastante espontánea al juntarse
-		unos pocos
-		<span class="text-orange-500">bitcoiners</span>
-		mayormente a través de esa maravilla que es
-		<span class="text-purple-500">nostr</span> para estar conectados compartiendo conocimientos y experiencias
-		para aprender los unos de los otros mediante el estudio y el uso de herramientas. No hay una estructura
-		definida, nos basamos en permanecer conectados entre nosotros tanto en línea como en encuentros periódicos.
-		Por un lado vamos comentando lo que vamos haciendo cada uno de nosotros y sobre lo que nos vendría
-		bien hablar y por otro hacemos puestas en común en los encuentros comentando avances y hablando sobre
-		algo que hayamos concretado. El contacto permanente posibilita la cooperación resolviendo necesidades
-		dentro del propio grupo, cada uno aporta lo que puede y se va creando esa infrastuctura a disposicion
-		de todos.
+		{$_("about.body1")}
+		<span class="text-orange-500">{$_("about.bitcoiners")}</span>
+		{$_("about.body2")}
+		<span class="text-purple-500">{$_("about.nostr")}</span> 
+		{$_("about.body3")}
 	</p>
 	<Popover {user} />
 </Card>
+
