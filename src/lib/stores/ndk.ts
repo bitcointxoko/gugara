cb7c749593bd6691f52512f8ef2fc1373ec3e13c
rasputin <ra5pvt1n@proton.me>
2024-12-13 19:09:54 +0100

    Switch back to relay list defined in .env

--- src/lib/stores/ndk.ts
+++ src/lib/stores/ndk.ts
@@ -1,21 +1,4 @@
-// import NDK from '@nostr-dev-kit/ndk';
-// import { browser } from '$app/environment';
-// import { PUBLIC_PUBKEY, PUBLIC_RELAYS } from '$env/static/public';
-
-// let relayUrls: string[];
-// relayUrls = PUBLIC_RELAYS.split(',');
-
-// // Create a new NDK instance with explicit relays
-// const ndk = new NDK({
-// 	explicitRelayUrls: relayUrls
-// });
-// // Now connect to specified relays
-// if (browser) {
-// 	ndk.connect().then(() => {
-// 		console.log('Connected');
-// 	});
-// }
-
+import { PUBLIC_PUBKEY, PUBLIC_RELAYS } from "$env/static/public";
 import { browser } from "$app/environment";
 import type { NDKCacheAdapter } from "@nostr-dev-kit/ndk";
 import NDKCacheAdapterDexie from "@nostr-dev-kit/ndk-cache-dexie";
@@ -23,26 +6,41 @@ import NDKSvelte from "@nostr-dev-kit/ndk-svelte";
 import NDK from "@nostr-dev-kit/ndk";
 import { writable } from "svelte/store";
 
+let relayUrls: string[];
+relayUrls = PUBLIC_RELAYS.split(",");
+
+// Create a new NDK instance with explicit relays
+// const ndk = new NDK({
+// explicitRelayUrls: relayUrls
+// });
+// Now connect to specified relays
+// if (browser) {
+// ndk.connect().then(() => {
+// console.log('Connected');
+// });
+// }
+
 let cacheAdapter: NDKCacheAdapter | undefined;
 
 if (browser) {
-    cacheAdapter = new NDKCacheAdapterDexie({ dbName: "nostorrents" });
+  cacheAdapter = new NDKCacheAdapterDexie({ dbName: "gugara" });
 }
 
 export const ndkStore = new NDKSvelte({
-    explicitRelayUrls: [
-        // 'wss://purplepag.es',
-        // 'wss://relay.nostr.band',
-        // 'wss://nos.lol',
-        // 'wss://relay.snort.social',
-        // 'wss://relay.damus.io',
-        // 'wss://nostr.wine',
-        // 'wss://bostr.bitcointxoko.com'
-        "ws://localhost:8080",
-    ],
-    outboxRelayUrls: ["wss://purplepag.es"],
-    enableOutboxModel: false,
-    cacheAdapter: cacheAdapter,
+  // explicitRelayUrls: [
+  // "wss://purplepag.es",
+  // "wss://relay.nostr.band",
+  // "wss://nos.lol",
+  // 'wss://relay.snort.social',
+  // "wss://relay.damus.io",
+  // "wss://nostr.wine",
+  // "wss://bostr.bitcointxoko.com",
+  // "ws://localhost:8080",
+  // ],
+  explicitRelayUrls: relayUrls,
+  outboxRelayUrls: ["wss://purplepag.es"],
+  enableOutboxModel: false,
+  cacheAdapter: cacheAdapter,
 });
 
 ndkStore.connect().then(() => console.log("NDK Connected"));
@@ -51,13 +49,13 @@ ndkStore.connect().then(() => console.log("NDK Connected"));
 const ndk = writable(ndkStore);
 
 export const bunkerNDKStore = new NDK({
-    explicitRelayUrls: [
-        "wss://relay.nsecbunker.com",
-        "wss://nos.lol",
-        "wss://relay.damus.io",
-        "wss://relay.nostr.band",
-    ],
-    enableOutboxModel: false,
+  explicitRelayUrls: [
+    "wss://relay.nsecbunker.com",
+    "wss://nos.lol",
+    "wss://relay.damus.io",
+    "wss://relay.nostr.band",
+  ],
+  enableOutboxModel: false,
 });
 
 bunkerNDKStore.connect().then(() => console.log("Bunker NDK Connected"));
