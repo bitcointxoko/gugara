66f20e6e65f64584b1fc8261ba94faa01398b9f5
rasputin <ra5pvt1n@proton.me>
2025-01-24 16:15:26 +0100

    add euskera translations

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -16,11 +16,7 @@
 		<a id="popover-0" href="/u/{user.npub}"
 			><span class="font-bold decoration-dotted hover:underline">{PUBLIC_NAME}</span>
 		</a>
-		{$_("about.body1")}
-		<span class="text-orange-500">{$_("about.bitcoiners")}</span>
-		{$_("about.body2")}
-		<span class="text-purple-500">{$_("about.nostr")}</span> 
-		{$_("about.body3")}
+		{$_("about.body")}
 	</p>
 	<Popover {user} />
 </Card>
